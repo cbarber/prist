fe2d0558d9dcca24577c5f3864a45d7d5c8e0c20
Craig Barber <craigb@mojotech.com>
2020-04-18 17:00:37 -0400

    feat: add CLI parsing for path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("prist")
+        .version("0.1.0")
+        .author("Craig Barber <craigb@mojotech.com>")
+        .about("Pull Request CLI")
+        .arg(
+            Arg::with_name("path")
+                .index(1)
+                .help("Sets path for the git repository"),
+        )
+        .get_matches();
+
+    let current_dir = std::env::current_dir().unwrap();
+    let current_dir = current_dir.to_str().unwrap();
+
+    let path = matches.value_of("path").unwrap_or(current_dir);
+
+    println!("{}", path);
 }
